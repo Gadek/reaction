60bfe6c8c922ea2e30d1c928f1ca2dfbfc78afd6
Gadek <radoslawgorski8@gmail.com>
2017-03-11 23:08:54 +0100

    with start button

--- sketch.js
+++ sketch.js
@@ -1,27 +1,29 @@
 function setup() {
   createCanvas(620,240);
-  ustalony_czas=floor(random(19,51));
-  frameRate(0);
+  againButton=createButton("START");
+  againButton.mousePressed(start);
+  againButton.position(10,10)
+  againButton.id("button");
   show_dist=createP("");
+  show_dist.id("distans");
+  show_dist.position(0,height*2/3)
   background(220);
   createLines();
 }
+
 function draw() {
-  if(!going) {
-    text("CLICK TO START",50,50);
-  }
   if(going) {
-  licznik++
+  licznik++;
   background(220);
   createLines();
   throwBall();
   setProperPosition();
  }
 }
+
 function mousePressed() {
-  if(endloop&&!checked) {
-    checkClick(mouseX)
+  if(throwBall() && !checked) {
+    checkClick(mouseX);
     checked=true;
   }
-  going=true
 }
